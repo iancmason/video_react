8325b8a830a34b06298029f56f8e93fdc20aa293
Ian <ian.christopher.mason@gmail.com>
2016-12-05 13:44:53 -0600

    filling out social video component

--- src/social-video.js
+++ src/social-video.js
@@ -1,9 +1,39 @@
-import React, {Component} from 'react';
+import qs from 'query-string';
+import React, {Component, PropTypes} from 'react';
 
 export default class SocialVideo extends Component {
+  static propTypes = {
+    service: PropTypes.oneOf(['youtube', 'vimeo', 'dailymotion']).isRequired,
+    video: PropTypes.string.isRequired
+  };
+
+  static urlMap = new Map([
+    ['youtube', 'http://www.youtube.com/embed/'],
+    ['vimeo', 'http://player.vimeo.com/video/'],
+    ['dailymotion', 'http://www.dailymotion.com/embed/video/']
+  ]);
+
+  getIdFromVideoString (vString) {
+    const urlArr = vString.split('/');
+    const idString = urlArr[urlArr.length - 1];
+    const queryParams = qs.extract(vString);
+
+    return (queryParams && qs.parse(queryParams).v) || idString || '';
+  }
+
   render() {
+    const {service, video, ...htmlTags} = this.props;
+    const src = `${SocialVideo.urlMap.get(service)}${this.getIdFromVideoString(video)}`;
+
     return (
-      <h1>Social Video</h1>
+      <iframe
+        src={src}
+        frameBorder='0'
+        webkitAllowFullScreen
+        mozallowfullscreen
+        allowFullScreen
+        {...htmlTags}
+      />
     );
   }
 }
